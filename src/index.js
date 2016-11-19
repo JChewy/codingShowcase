1c482c0f2f51cc8ce8b0fd9bea5d2673ae34ac4c
JChewy <retro3hat@gmail.com>
2016-11-19 15:42:58 -0500

    video and search function

--- src/index.js
+++ src/index.js
@@ -1,2 +1,48 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search'; 
+import SearchBar from './components/searchbar'; 
+import VideoList from './components/video_list'; 
+import VideoDetail from './components/video_detail';
+const API_KEY = 'AIzaSyA_1qpu2HrKoCjCTjIV3oVsmsfy_iZnUu4';
+
+
+
+
+class App extends Component {
+	constructor(props){
+		super(props); 
+
+		this.state = { 
+						videos:[], 
+						selectedVideo: null, 
+					}; 
+
+		this.videoSearch('chess'); 
+	}
+
+	videoSearch(term) {
+		YTSearch({key: API_KEY, term: term}, (videos) => {
+			this.setState({ 
+				videos: videos, 
+				selectedVideo: videos[0]
+			}); 
+		}); 
+	}
+
+
+	render(){
+		return(
+			<div>
+				<SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+				<VideoDetail video={this.state.selectedVideo} />
+				<VideoList 
+					videos={this.state.videos} 
+					onVideoSelect={selectedVideo => this.setState({selectedVideo})}/>
+			</div>
+		); 
+	}
+}
+
+
+ReactDOM.render(<App />, document.querySelector('.container'));
